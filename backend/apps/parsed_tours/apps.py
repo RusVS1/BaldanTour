24810c2c27c050065b37c99a2eb592b922cbe362
MinikoCappu <96332603+MinikoCappu@users.noreply.github.com>
2026-03-22 01:38:17 +0800

    Delete backend/apps directory

--- backend/apps/parsed_tours/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParsedToursConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.parsed_tours"
-
--- backend/apps/search_history/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import SearchHistoryListCreateView
-
-urlpatterns = [
-    path('', SearchHistoryListCreateView.as_view(), name='search-history'),
-]
