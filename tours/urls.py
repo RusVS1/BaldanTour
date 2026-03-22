2bb07110c4e5a022f820b246c5ba4dcb39dbcbab
MinikoCappu <96332603+MinikoCappu@users.noreply.github.com>
2026-03-22 16:28:50 +0800

    Delete tours directory

--- tours/urls.py
+++
@@ -1,9 +0,0 @@
-﻿from django.urls import path
-
-from .views import TourDetailView, TourListView, TourSemanticSearchView
-
-urlpatterns = [
-    path('', TourListView.as_view(), name='tour-list'),
-    path('search/', TourSemanticSearchView.as_view(), name='tour-search'),
-    path('<int:pk>/', TourDetailView.as_view(), name='tour-detail'),
-]
